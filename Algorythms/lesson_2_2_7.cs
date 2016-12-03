96ed038838c523ab6eacd5e96982158500235142
Fynta <Fynta@PORTABLEFYNTA>
2016-12-03 17:48:29 +0300

    lesson 2-2-7

---
+++ Algorythms/lesson_2_2_7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algorythms
+{
+    class lesson_2_2_7
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            long n;
+            if (long.TryParse(input.ToString(), out n))
+            {
+                long[] fibArr = new long[n + 1];
+                for (int i = 0; i <= n; i++)
+                {
+                    if (i < 2)
+                    {
+                        fibArr[i] = i;
+                        continue;
+                    }
+                    fibArr[i] = (fibArr[i - 1] + fibArr[i - 2]) % 10;
+                }
+                Console.WriteLine(fibArr[n]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
