cc1bc63fc4450e67d8f25bf9521a4b7c0880cf22
Fynta <Fynta@PORTABLEFYNTA>
2016-12-03 19:08:35 +0300

    class modified

--- Algorythms/Program.cs
+++ Algorythms/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algorythms
+﻿namespace Algorythms
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             lesson_2_2_6.GetFibNum();
         }
