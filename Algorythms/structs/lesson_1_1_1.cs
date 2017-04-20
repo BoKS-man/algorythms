9e793ef6e02464246c8f06168abc59952d7f63ba
BoKS <boks24@ya.ru>
2017-04-20 15:29:27 +0300

    1

---
+++ Algorythms/structs/lesson_1_1_1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorythms.structs
+{
+    class lesson_1_1_1
+    {
+        public string test(string input)
+        {
+            return string.Empty;
+        }
+    }
+}
